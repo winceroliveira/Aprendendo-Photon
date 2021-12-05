481e1ed016ab18638dee21ac8a26b92b89341a0d
Wincer Oliveira <85763225+winceroliveira@users.noreply.github.com>
2021-12-05 18:39:32 -0300

    criando sala com nome e login
    criando sala com nome e login

--- Assets/Scripts/NetworkController.cs
+++ Assets/Scripts/NetworkController.cs
@@ -3,51 +3,97 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.UI;
 
 public class NetworkController : MonoBehaviourPunCallbacks
 {
     public string servidor;
     public bool isConected = false;
+    public GameObject Login;
+    public GameObject Partidas;
+    public InputField usuario;
+    public Text usuarioNome;
+    public InputField nomeSala;
+
+    string m_nomeUsuario = string.Empty;
+    [HideInInspector]
+    public string nomeUsuario
+    {
+        get => m_nomeUsuario;
+        set
+        {
+            m_nomeUsuario = value.Trim();
+            usuario.text = m_nomeUsuario;
+        }
+
+    }
+
+    string m_nomeSala = string.Empty;
+    [HideInInspector]
+    public string NomeSala
+    {
+        get => m_nomeSala;
+        set
+        {
+            m_nomeSala = value;
+            nomeSala.text = m_nomeSala;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings();
-        
+
+
     }
-
-
-    // EVENTO Q RETORNA QUANTO O JOGADOR É CONECTADO
-    public override void OnConnected()
+    public void Logar()
     {
-        Debug.Log("Conectado");
-        isConected = true;
+        if (usuario.text != string.Empty)
+        {
+            Login.SetActive(false);
+            Partidas.SetActive(true);
+            usuarioNome.text = nomeUsuario;
+            PhotonNetwork.ConnectUsingSettings();
+
+        }
+        else
+        {
+            Debug.Log("Sem nome de Usuário");
+        }
+
     }
 
-
-    //EVENTO Q RETORNA QUANDO O JOGADOR FOI CONECTADO E FOI VALIDADO COMO MASTER
     public override void OnConnectedToMaster()
     {
-        Debug.Log("Conectado como Master");
-        Debug.Log("Server: " + PhotonNetwork.CloudRegion + "   Ping: " + PhotonNetwork.GetPing());
-
-        //criando lobby
-        PhotonNetwork.JoinLobby();
-
+        Debug.Log("Conectado como master");
+        CriarLobby();
     }
+    public void CriarLobby()
+    {
+        
 
-    //CONSEGUIU ENTRAR NA SALA
+        OnJoinedLobby();
+    }
+    //CONSEGUIU ENTRAR NO LOBBY
     public override void OnJoinedLobby()
     {
-        PhotonNetwork.JoinRandomRoom();
         Debug.Log("Entrou no lobby");
+    }
+
+    public void BuscarPartida()
+    {
+        PhotonNetwork.JoinRandomRoom();
+
     }
 
     //EVENTO DE ERRO AO ENTRAR SALAS
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
-        string roomTemp = "Room: " + Random.Range(1,10000);
+        Debug.Log("Não existe nenhuma sala");
+    }
 
-        PhotonNetwork.CreateRoom(roomTemp);
+    public void CriarSala()
+    {
+        PhotonNetwork.CreateRoom(nomeSala.text);
     }
 
     //EVENTO DE SUCESSO AO ENTRAR NA SALAs
@@ -64,3 +110,5 @@ public class NetworkController : MonoBehaviourPunCallbacks
         isConected = false;
     }
 }
+
+
